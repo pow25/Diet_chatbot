9d1707d82df15ecd570b2ef5d5489e610de6128a
Ericlung <lungkunhung@gmail.com>
2017-11-01 02:25:43 +0800

    '???'

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/OcrApiController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/OcrApiController.java
@@ -5,16 +5,15 @@ import java.io.File;
 import java.net.*;
 
 public class OcrApiController {
-	private final String FILENAME = "/static/test-image.png";
+	private final String FILEURL = "http://asprise.com/ocr/img/test-image.png";
 	
 	public String test() {
 		Ocr.setUp(); // one time setup
 		Ocr ocr = new Ocr(); // create a new OCR engine
 		ocr.startEngine("eng", Ocr.SPEED_SLOW); // English
 		String testing_result = new String();
-//		URL[] testURL = {new URL("http://asprise.com/ocr/img/test-image.png")};
 		try {
-			testing_result = ocr.recognize(new File[] {new File(FILENAME)}, Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT);
+			testing_result = ocr.recognize(new URL[] {new URL(FILERUL)}, Ocr.RECOGNIZE_TYPE_ALL, Ocr.OUTPUT_FORMAT_PLAINTEXT);
 			ocr.stopEngine();
 		} catch (Exception e) {
 			return e.toString();
