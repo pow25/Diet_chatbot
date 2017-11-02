912f3a39a44519513921518eeb46835d5dd445d6
Ericlung <lungkunhung@gmail.com>
2017-11-02 14:55:32 +0800

    ok

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -128,12 +128,10 @@ public class KitchenSinkController {
 			throw new RuntimeException(e);
 		}
 		DownloadedContent jpg = saveContent("jpg", response);
-		
-		OcrApiController test= new OcrApiController();
-    	String output = test.test("Img",jpg.getUri());
-    	this.replyText(replyToken,"Testing output: " + output);
-	}
+		reply(((MessageEvent) event).getReplyToken(), new ImageMessage(jpg.getUri(), jpg.getUri()));
 
+	}
+	
 	@EventMapping
 	public void handleAudioMessageEvent(MessageEvent<AudioMessageContent> event) throws IOException {
 		final MessageContentResponse response;
@@ -212,10 +210,11 @@ public class KitchenSinkController {
 	}
 
 	private void handleTextContent(String replyToken, Event event, TextMessageContent content)
-            throws Exception {
+throws Exception {
         String text = content.getText();
-
+        
         log.info("Got text message from {}: {}", replyToken, text);
+
         switch (text) {
             case "profile": {
                 String userId = event.getSource().getUserId();
@@ -228,68 +227,91 @@ public class KitchenSinkController {
                 }
                 break;
             }
-            case "confirm": {
-                ConfirmTemplate confirmTemplate = new ConfirmTemplate(
-                        "Do it?",
-                        new MessageAction("Yes", "Yes!"),
-                        new MessageAction("No", "No!")
-                );
-                TemplateMessage templateMessage = new TemplateMessage("Confirm alt text", confirmTemplate);
-                this.reply(replyToken, templateMessage);
-                break;
-            }
-            case "ocr": {
-            	OcrApiController test= new OcrApiController();
-            	String output = test.test("URL","");
-            	this.replyText(replyToken,"Testing output: " + output);
+//            case "confirm": {
+//                ConfirmTemplate confirmTemplate = new ConfirmTemplate(
+//                        "Do it?",
+//                        new MessageAction("Yes", "Yes!"),
+//                        new MessageAction("No", "No!")
+//                );
+//                TemplateMessage templateMessage = new TemplateMessage("Confirm alt text", confirmTemplate);
+//                this.reply(replyToken, templateMessage);
+//                break;
+//            }
+            case "menu":{
+            	
             	break;
             }
-            case "ocr2": {
-            	String imageUri = createUri("/static/test-image.png");
-            	this.reply(replyToken, new ImageMessage(imageUri, imageUri));
+            
+            case "input":{
+            	
             	break;
             }
-            case "ocr3": {
-            	OcrApiController test= new OcrApiController();
-            	String output = test.test("Default","");
-            	this.replyText(replyToken,"Testing output: " + output);
+            
+            case "create":{
+            	
             	break;
             }
-            case "carousel": {
-                String imageUrl = createUri("/static/buttons/1040.jpg");
-                CarouselTemplate carouselTemplate = new CarouselTemplate(
-                        Arrays.asList(
-                                new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
-                                        new URIAction("Go to line.me",
-                                                      "https://line.me"),
-                                        new PostbackAction("Say hello1",
-                                                           "hello1")
-                                )),
-                                new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
-                                        new PostbackAction("çŒ«ç¯“éˆ§ï¿½ hello2",
-                                                           "hello",
-                                                           "hello2"),
-                                        new MessageAction("Say message",
-                                                          "Rice")
-                                ))
-                        ));
-                TemplateMessage templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
-                this.reply(replyToken, templateMessage);
-                break;
+            case "hi":{
+               String reply = null;
+//            String userid = event.getSource().getUserId();
+               String user_name = "czhangar";
+        	//first, search whether the user is a new user or not
+//            try {
+//        		user_name = database.search(userid);
+//        		user_name=lineMessagingClient.getProfile(userid);
+//        	} catch (Exception e) {
+//        		reply = "Welcome to the diet chatbot, please type: 'create' to create your personal file";
+//        		break;
+//        	}
+               reply = "Welcome to the diet chatbot!";
+               reply += user_name;
+               reply += '\n';
+               reply += "There are several functions you can use:";
+               reply += '\n';
+               reply += "Keyword: profile ";
+               reply += '\n';
+               reply += "It will provide you the personal health information";
+               reply += '\n';
+               reply += "Keyword: menu ";
+               reply += '\n';
+               reply += "It will offer you the advised menu for your meal based on your personal infomation";
+               reply +="Keyword: input ";
+               reply += '\n';
+               reply += "After type 'input', you can input the text,image or url as you wish. The chatbot will reply related information. ";
+               this.replyText(replyToken, reply);	
+
+             break;
             }
+//            case "carousel": {
+//                String imageUrl = createUri("/static/buttons/1040.jpg");
+//                CarouselTemplate carouselTemplate = new CarouselTemplate(
+//                        Arrays.asList(
+//                                new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
+//                                        new URIAction("Go to line.me",
+//                                                      "https://line.me"),
+//                                        new PostbackAction("Say hello1",
+//                                                           "hello")
+//                                )),
+//                                new CarouselColumn(imageUrl, "hoge", "fuga", Arrays.asList(
+//                                        new PostbackAction()
+//                                        new MessageAction("Say message",
+//                                                          "Rice")
+//                                ))
+//                        ));
+//                TemplateMessage templateMessage = new TemplateMessage("Carousel alt text", carouselTemplate);
+//                this.reply(replyToken, templateMessage);
+//                break;
+//            }
 
             default:
             	String reply = null;
             	try {
             		reply = database.search(text);
             	} catch (Exception e) {
-            		reply = text;
+            		reply = "We couldn't find the usuful information about your input, please type hi to get started";
             	}
                 log.info("Returns echo message {}: {}", replyToken, reply);
-                this.replyText(
-                        replyToken,
-                        itscLOGIN + " says " + reply
-                );
+                this.replyText(replyToken,reply);
                 break;
         }
     }
