315ea5d0742ed78006c8b52414670caf954cd2f6
Ericlung <lungkunhung@gmail.com>
2017-11-02 23:27:30 +0800

    ?

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -129,7 +129,7 @@ public class KitchenSinkController {
 		}
 		DownloadedContent jpg = saveContent("jpg", response);
 		OcrApiController OcrApi = new OcrApiController();
-		String imageText = OcrApi.recognize("IMG",jpg.getUri());
+		String imageText = OcrApi.recognize("URL",jpg.getUri());
 		this.replyText(replyToken, "Recognized message:"+jpg.getUri()+"   "+imageText);
 	}
 	
