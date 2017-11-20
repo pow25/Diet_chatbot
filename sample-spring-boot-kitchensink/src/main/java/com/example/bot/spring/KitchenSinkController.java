f887baf0cee72459ac1e119414d1249d58045bd2
wyh <ywangdq.gmail.com>
2017-11-20 19:20:13 +0800

    client and menu class test case finished

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -628,9 +628,12 @@ throws Exception{
                 try  {
                 	String reply = null;
                 	reply = mymenu.getRecommendServing(client.getGender(),clientagerange,false);
-                	this.replyText(replyToken,reply);
+                	if (reply!=null)
+                		this.replyText(replyToken,reply);
+                	else
+                		this.replyText(replyToken, "Can't get recommended serving,something wrong!");
                 } catch(Exception e)  {
-                    this.replyText(replyToken, "Bot can't use profile,something wrong!");
+                    this.replyText(replyToken, "Can't get recommended serving,something wrong!");
                 }
                 break;
             }
